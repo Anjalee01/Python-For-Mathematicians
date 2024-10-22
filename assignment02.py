757c9f891c2b8205bbddedb418497171c37ee432
Anjalee01 <anjaleeramwani01@gmail.com>
2024-10-22 23:22:47 +0500

    assignment 1 and 2 completed

---
+++ assignment02.py
@@ -0,0 +1,30 @@
+PYTHON FOR MATHEMATICIANS
+Assignment # 2
+What is the purpose of indentation in Python?		( /5)
+
+
+How do you write a comment in Python?			( /1)	
+
+
+Correct the given code:						( /2)
+x = 5
+print(x / 0)
+print this pattern							( /3)
+*
+**
+***
+****
+*****
+What will be the output of this code?				( /2)
+x = 10
+y = x + 5
+print(x, y)
+
+Declare a string variable greeting and assign it the value "Hello, Python!". Print the string.			( /3)
+Create three variables: a, b, and c. Assign a a value of 10, b a value of 3.5, and c the sum of a and b. Print the value of c and its data type.								( /5)
+What will be the output of the following code and why?	( /5)
+x = 5
+y = "5"
+print(x == y)
+print(x + int(y))
+
